5a78680c733837e629220445cb4b8b2adeb62638
nikhil7174 <nikhilkumarsingh7174@gmail.com>
2025-12-06 01:18:15 +0530

    allowed render url for cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -27,8 +27,8 @@ const PORT = process.env.PORT || 3001;
 app.use(helmet());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production'
-    ? ['https://crisp-beta.vercel.app']
-    : ['http://localhost:5173','http://localhost:5174', 'http://localhost:3000', 'https://crisp-beta.vercel.app', 'https://crisp-7l8d2q9ft-nikhil7174s-projects.vercel.app'],
+    ? ['https://crisp-beta.vercel.app', 'https://crisp-1.onrender.com']
+    : ['http://localhost:5173','http://localhost:5174', 'http://localhost:3000', 'https://crisp-beta.vercel.app', 'https://crisp-7l8d2q9ft-nikhil7174s-projects.vercel.app', 'https://crisp-1.onrender.com'],
   credentials: true
 }));
 app.use(morgan('combined'));
